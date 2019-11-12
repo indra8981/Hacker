5f5d2a193df55aeb49aa347c39295fd51a0e2e54
Indrajit Sinha <37447050+indra8981@users.noreply.github.com>
2019-11-12 14:01:41 +0530

    Create Imp.java

---
+++ Imp.java
@@ -0,0 +1,10 @@
+new Thread(null, new Runnable() {
+            public void run() {
+                try{
+                    //Insert code here
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }, "1", 1 << 26).start();
